a4cfa5d4bf1d71afc6083808198767eaa692013f
Naman Bakshi <namanbakshi2004@gmail.com>
2025-02-20 04:27:27 +0530

    Quick setup

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from packet_sniffer import start_sniffing
+#from protocol_parser import parse_packet
+#from analysis import analyze_packet
+
+#def process_packet(raw_data):
+
+
+def main():
+    print("Network Monitor Project Initialized.")
+
+if __name__ == "__main__":
+    main()
---
+++ src/packet_sniffer.py
@@ -0,0 +1,7 @@
+import socket
+
+def start_sniffing(callback):
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+        raw_data, addr = conn.recvfrom(65535)
+        callback(raw_data)
